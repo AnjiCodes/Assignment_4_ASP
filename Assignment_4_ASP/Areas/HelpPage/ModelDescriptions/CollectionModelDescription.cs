bbe938b272153b6dbe8b58995e4279f7cf5ed2ac
AnjiCodes <n01579648@humber.ca>
2023-04-11 09:18:08 -0400

    Add project files.

---
+++ Assignment_4_ASP/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Assignment_4_ASP.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Assignment_4_ASP/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Assignment_4_ASP.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
